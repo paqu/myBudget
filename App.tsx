4abd0d2e91faf8701df1cfde95019fcdc00b758c
Pawel Kusz <pawel.kusz1993@gmail.com>
2025-09-16 12:58:34 +0200

    Update entry point to application

--- App.tsx
+++ App.tsx
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { LogBox } from "react-native";
+import NavigationProvider from "./src/navigation";
+
+// Ignore specific warnings (optional)
+LogBox.ignoreLogs([
+  "Warning: AsyncStorage has been extracted from react-native",
+  "Warning: componentWillReceiveProps has been renamed",
+  "Remote debugger", // Firebase warnings
+]);
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <NavigationProvider />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
